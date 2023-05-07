aee85de14ec45b5fde08c7a27728066ea217efdd
Gabriel Matos <gabrielmatos010203@gmail.com>
2023-05-06 23:55:46 -0300

    chore: icones e configuração do react-router

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
 export function Rotas() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Home />} />
         <Route path="/dashboard" element={<Home />} />
         <Route path="/adm" element={<Home />} />
