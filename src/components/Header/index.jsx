9852ee6c2d7c1dbb41c573d98aa663c3b8719e32
Gabriel Matos <gabrielmatos010203@gmail.com>
2023-05-06 23:58:12 -0300

    feat: componente de header da pagina de home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,24 @@
+/* eslint-disable react/prop-types */
 import { LogoutBtn } from "../LogoutBtn";
 import { SearchHeader } from "../SearchHeader";
+import styles from "./styles.module.scss";
+import user from "../../assets/user.svg";
+import logo from "../../../public/logo.svg";
 
-export function HeaderHome() {
+export function HeaderHome({ setIsSearchFocused }) {
   return (
-    <header>
-      <div>
-        <img src="" alt="" />
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src={logo} alt="Logo" />
         <nav>
-          <a href="">Home</a>
-          <a href="">Historico</a>
+          <a href="#">Home</a>
+          <a href="#">Historico</a>
         </nav>
-        <SearchHeader />
-        <LogoutBtn />
+        <div className={styles.utilsContent}>
+          <SearchHeader setIsSearchFocused={setIsSearchFocused} />
+          <img src={user} alt="Usuario" />
+          <LogoutBtn />
+        </div>
       </div>
     </header>
   );
