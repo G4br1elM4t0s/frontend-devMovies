cc788152d44ddc5b6b3a55b14dfb7fc069240929
G4br1elM4t0s <gabrielmatos010203@gmail.com>
2023-05-25 17:05:30 -0300

    feat: Começando a fazer os cards

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.scss";
+
+export function Card() {
+  return (
+    <>
+      <div className={styles.cardBox}>
+        <img src="" alt="" />
+        <h1>Title aqui</h1>
+      </div>
+    </>
+  );
+}
